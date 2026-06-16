711390b4908e51274d652247549e18ba81026ad8
ZHIXIANGYOU <p96091050@gs.ncku.edu.tw>
2026-06-16 16:24:52 +0800

    fix utf-8

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const requestListener = (req,res) =>{
         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json; charset=utf-8'
     }
 
     let body = "";
